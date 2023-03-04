ebd91dab4ffcdc4914b59fbe289f720abdbfdfb2
Nguyenductrinh <86778595+Nguyenductrinh@users.noreply.github.com>
2023-03-04 10:44:02 +0700

    add

--- btth02v2/configs/DBConnection.php
+++ btth02v2/configs/DBConnection.php
@@ -6,7 +6,7 @@ class DBConnection{
     public function __construct(){
          // B1. Kết nối DB Server
          try {
-            $this->conn = new PDO('mysql:host=localhost;dbname=demo_;port=3306', 'root','btth01_cse485');
+            $this->conn = new PDO('mysql:host=localhost;dbname=demo_;port=3306', 'root','');
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
